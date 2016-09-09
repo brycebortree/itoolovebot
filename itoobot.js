4e8e2c6f1dc26906b13a90e002ec120169b9afe4
brytree <brycebortree@gmail.com>
2016-09-09 16:07:03 -0700

    first commit

---
+++ itoobot.js
@@ -0,0 +1,12 @@
+var _           = require('lodash');
+var Client      = require('node-rest-client').Client;
+var Twit        = require('twit');
+var async       = require('async');
+var wordFilter  = require('wordfilter');
+
+var t = new Twit({
+  consumer_key: 
+  consumer_secret:
+  access_token:
+  access_token_secret:
+})
