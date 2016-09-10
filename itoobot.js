8eb584d92c88dcab8a1fc2a28299d1b6a1f62375
brytree <brycebortree@gmail.com>
2016-09-09 18:31:03 -0700

    adding express due to heroku error

--- itoobot.js
+++ itoobot.js
@@ -4,7 +4,19 @@ var Twit        = require('twit');
 var async       = require('async');
 var wordFilter  = require('wordfilter');
 var env         = require('dotenv').config();
+var express     = require('express');
+var app         = express();
 
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 var t = new Twit({
   consumer_key: env.CONSUMER_KEY,
